5b2018685fbeca2f2f31e511111329f817c1ba3c
Dev.Harsh <157444325+harshad4558@users.noreply.github.com>
2025-05-13 20:18:58 +0530

    Update AuthForm.jsx

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -35,7 +35,7 @@ export default function AuthForm({ type }) {
 
     try {
       // const res = await fetch(`http://localhost:8080/api/auth/${endpoint}`
-        const res = await fetch('https://ai-resume-builder-backend-3qrk.onrender.com', {
+        const res = await fetch("https://ai-resume-builder-backend-3qrk.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dataToSend),
